69f8a2bd9c9cc1f3f5ecc0fe15b7e9a5b3e15fc4
Fernando Colom <colomfernando@gmail.com>
2019-03-23 20:15:49 -0300

    add eslint airbnb. Create App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GlobalStyles from 'GlobalStyles';
+
 class App extends Component {
   render() {
     return (
---
+++ src/components/App/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components/macro';
+
+export default {
+  Container: styled.div`
+    display: grid;
+  `,
+};
